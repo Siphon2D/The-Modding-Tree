a43a8c5aff605e6cbd8b05db88ba7d18c8f2b5d0
Siphon2D <max.broyles@nrcs.net>
2025-08-16 15:06:56 -0400

    Revert "1 LI UPDATE!!"
    This reverts commit c3325d592ab0bab31ea07282ea99b6dc0e8ab8f2.

--- js/layers.js
+++ js/layers.js
@@ -15,8 +15,6 @@ addLayer("e", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-         if (hasUpgrade('e', 13)) mult = mult.times(upgradeEffect('e', 13))
-        if (hasUpgrade('e', 14)) mult = mult.times(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -51,49 +49,12 @@ addLayer("e", {
               return player.points.add(1).pow(0.15)
            },
            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+           gainMult() {
+           let mult = new Decimal(1)
+           if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
+           return mult
+           },
         },
-        14: {
-           title: "Dozen Eggs",
-           description: "Double your egg gain.",
-           cost: new Decimal(12),
-
-        },
-        15: {
-           title: "Only 5 Upgrades?!",
-           description: "Unlock 1LI",
-           cost: new Decimal(48),
-        },
+        
     },
 })
-addLayer("1", {
-    name: "1li", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "1", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: false,
-		eggs: new Decimal(48),
-    }},
-    color: "#FF0000",
-    requires: new Decimal(50), // Can be a function that takes requirement increases into account
-    resource: "1st LI", // Name of prestige currency
-    baseResource: "eggs", // Name of resource prestige is based on
-    baseAmount() {return player.eggs}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "1", description: "1: Reset for 1st LI", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true},
-     upgrades: {
-       
-    },
-})
-
